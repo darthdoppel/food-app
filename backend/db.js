79a0296e03f22c33f8b238ec290251bcd79fb927
Leandro Magallanes <leandromartinm7@gmail.com>
2023-10-14 16:59:20 -0300

    backend updates

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,20 @@
+// db.js
 import mysql from 'mysql2/promise'
 import { config } from 'dotenv'
 config()
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL) // Add this line
+console.log('DATABASE_URL:', process.env.DATABASE_URL)
 
-export const connection = await mysql.createConnection(process.env.DATABASE_URL)
+let connection
+
+export const connectToDatabase = async () => {
+  try {
+    connection = await mysql.createConnection(process.env.DATABASE_URL)
+    console.log('Connected to database')
+  } catch (error) {
+    console.error('Could not connect to database:', error)
+    process.exit(1)
+  }
+}
+
+export { connection }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+// server.js
+import { config } from 'dotenv'
+
 import app from './app.js'
+import { connectToDatabase } from './db.js'
 import pkg from 'picocolors'
+config()
 const { green } = pkg
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(green(`Server running on port ${port}`)))
+// Connect to the database before starting the server
+connectToDatabase().then(() => {
+  app.listen(port, () => console.log(green(`Server running on port ${port}`)))
+})
