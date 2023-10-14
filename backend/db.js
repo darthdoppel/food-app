7c07211f5865a8bf88ca968df98113381c650079
Leandro Magallanes <leandromartinm7@gmail.com>
2023-10-14 16:46:16 -0300

    console.log for check

--- backend/db.js
+++ backend/db.js
@@ -2,4 +2,6 @@ import mysql from 'mysql2/promise'
 import { config } from 'dotenv'
 config()
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL) // Add this line
+
 export const connection = await mysql.createConnection(process.env.DATABASE_URL)
